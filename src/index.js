3aa54de3c43c30645e76c72d982440f3e89ef771
Fedo Viktor <fedviktor@bitbucket.org>
2020-10-17 18:22:19 +0300

    feat: add file src/index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,16 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+
+    let towelSort=[];
+if (!matrix) {
+    return []
+  }
+    for (let i = 1; i < matrix.length; i+=2) {
+         matrix[i]=matrix[i].reverse()
+    }
+         let sort=matrix.flat()
+ return sort
+     }
+
+
